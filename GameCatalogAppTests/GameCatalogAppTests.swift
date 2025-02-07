6c609b8744335343332fe8d868fd252e76e892ba
Fachmi Dimas Ardhana <fachmi2007@gmail.com>
2025-02-08 01:05:46 +0700

    add initial unit test file

---
+++ GameCatalogAppTests/GameCatalogAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  GameCatalogAppTests.swift
+//  GameCatalogAppTests
+//
+//  Created by Fachmi Dimas Ardhana on 08/02/25.
+//
+
+import Testing
+
+struct GameCatalogAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
